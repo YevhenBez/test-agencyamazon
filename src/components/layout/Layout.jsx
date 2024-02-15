13994175cb2457fdffa53b4a0ffa2ac7d15908a7
YevhenBez <byxgalter@bigmir.net>
2024-02-15 15:03:42 +0200

    Причёсываю под данное тестовое своё предыдущее тестовое. Убрал    header   и всё, что связано с ним. Тестовое для бота Agencyamazon.com   в Телеграмме.
    Причёсываю под данное тестовое своё предыдущее тестовое. Убрал    header   и всё, что связано с ним. Для этого я удалил фотку   helloEvano.jpg   из файла   jpg     Затем я удалил импорт данной фотки в файл   Layout.jsx    и также удалил разметку       header   в файле   Layout.jsx    Затем удалил уже не нужные стили для    header    в файле     layout.module.css      Тестовое для бота Agencyamazon.com   в Телеграмме.

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,7 +2,6 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../../components/sidebar';
 import css from '../../components/layout/css/layout.module.css';
-import helloPhoto from '../../img/jpg/helloEvano.jpg';
 
 const Layout = () => {
   return (
@@ -11,10 +10,7 @@ const Layout = () => {
         <Sidebar />
       </div>
       <div className={css.layoutPage__container}>
-        <header className={css.layoutPage__container__header}>
-          <img src={helloPhoto} alt="hello" width={176} height={36} />
-        </header>
-
+        
         <Suspense>
           <Outlet />
         </Suspense>
