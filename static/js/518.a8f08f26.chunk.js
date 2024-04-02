73d1970a787d38aa7e04e1b5452f5475c136bc1a
YevhenBez <115114239+YevhenBez@users.noreply.github.com>
2024-04-02 22:00:02 +0000

    Deploying to gh-pages from @ YevhenBez/test-agencyamazon@fbcf7fb4d298ad1465f6e9885d6d6c550a9156d1 🚀

---
+++ static/js/518.a8f08f26.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[518],{1518:function(e,t,n){n.r(t),n.d(t,{default:function(){return s}});var r=n(184),s=function(){return(0,r.jsx)("div",{style:{height:"100vh",justifyContent:"center",alignItems:"center",fontSize:40,color:"#010101"},children:(0,r.jsx)("h2",{children:"The development of these pages is not included in this test task."})})}}}]);
+//# sourceMappingURL=518.a8f08f26.chunk.js.map
